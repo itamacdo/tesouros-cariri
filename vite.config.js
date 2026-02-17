406d030dc431a0330cf914c1548bf1a465c3d6a1
itamacdo <itamaria789@gmail.com>
2026-02-17 19:08:59 -0300

    atualizaçao pasta

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite' // Esta é a linha que está faltando!
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/tesouros-cariri/', 
