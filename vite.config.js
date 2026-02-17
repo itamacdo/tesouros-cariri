fd79005c817679d68843ce6c6a7457826aee641b
itamacdo <itamaria789@gmail.com>
2026-02-17 19:17:22 -0300

    feat: versão final com mapa panorâmico e glossário regional

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite' // Esta é a linha que está faltando!
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
